f5cebbb364651df6b40dbf7354b42dbad16b44a4
Andyqian7941 <50451666+Andyqian7941@users.noreply.github.com>
2026-03-24 19:35:41 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,85 +1,173 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct block{
-    vector<pair<int,int>> v;
-    block rotate(){
-        block ret;
-        for(auto [x,y]:v)
-        ret.v.push_back({y,-x});
-        int mx=0,my=0;
-        for(auto [x,y]:ret.v)
-            mx=min(mx,x),my=min(my,y);
-        for(auto &[x,y]:ret.v)
-            x-=mx,y-=my;
-        return ret;
-    }
-    block flip(){
-        block ret;
-        for(auto [x,y]:v)
-        ret.v.push_back({-x,y});
-        int mx=0,my=0;
-        for(auto [x,y]:ret.v)
-            mx=min(mx,x),my=min(my,y);
-        for(auto &[x,y]:ret.v)
-            x-=mx,y-=my;
-        return ret;
-    }
-    operator __int128(){
-        __int128 ret=0;
-        for(auto [x,y]:v){
-            ret|=__int128(1)<<(x*10+y);
+using U128 = __int128;
+struct Block
+{
+    vector<pair<int, int>> cells;
+    Block rotate() const
+    {
+        Block ret;
+        ret.cells.reserve(cells.size());
+        for (auto [x, y] : cells)
+            ret.cells.emplace_back(y, -x);
+        int minx = INT_MAX, miny = INT_MAX;
+        for (auto [x, y] : ret.cells)
+        {
+            minx = min(minx, x);
+            miny = min(miny, y);
+        }
+        for (auto &[x, y] : ret.cells)
+        {
+            x -= minx;
+            y -= miny;
         }
         return ret;
     }
-}a[]{{{{0,0},{0,1},{1,0},{1,1},{2,0}}},{{{2,0},{1,0},{1,1},{0,1},{0,2}}},{{{0,0},{1,0},{2,0},{2,1},{2,2}}},{{{0,0},{1,0},{0,1}}},{{{0,0},{1,0},{2,0},{2,1}}},{{{1,0},{1,1},{0,1},{2,1},{1,2}}},{{{0,0},{1,0},{0,1},{1,1}}},{{{0,0},{1,0},{0,1},{0,2},{1,2}}},{{{0,0},{1,0},{1,1},{1,2},{1,3}}},{{{0,0},{0,1},{0,2},{0,3}}},{{{0,0},{0,1},{0,2},{0,3},{1,2}}},{{{0,1},{1,1},{2,1},{2,0},{3,0}}}};
-vector<__int128> v[12];
-__int128 all;
-unordered_map<__int128,int> f[110][1<<12];
-int main(){
-    auto st=clock();
-    for(int i=0;i<10;i++)
-    for(int j=0;j+i<10;j++)
-    all|=__int128(1)<<(i*10+j);
-    for(int i=0;i<12;i++){
-        v[i].push_back(a[i]);
-        v[i].push_back(a[i].rotate());
-        v[i].push_back(a[i].rotate().rotate());
-        v[i].push_back(a[i].rotate().rotate().rotate());
-        v[i].push_back(a[i].flip());
-        v[i].push_back(a[i].flip().rotate());
-        v[i].push_back(a[i].flip().rotate().rotate());
-        v[i].push_back(a[i].flip().rotate().rotate().rotate());
-        sort(v[i].begin(),v[i].end()),v[i].resize(unique(v[i].begin(),v[i].end())-v[i].begin());
+    Block flip() const
+    {
+        Block ret;
+        ret.cells.reserve(cells.size());
+        for (auto [x, y] : cells)
+            ret.cells.emplace_back(-x, y);
+        int minx = INT_MAX, miny = INT_MAX;
+        for (auto [x, y] : ret.cells)
+        {
+            minx = min(minx, x);
+            miny = min(miny, y);
+        }
+        for (auto &[x, y] : ret.cells)
+        {
+            x -= minx;
+            y -= miny;
+        }
+        return ret;
     }
-    f[0][0][0]=1;
-    __int128 cur=0;
-    for(int i=0;i<100;i++){
-        for(int j=0;j<1<<12;j++){
-            for(int k=0;k<12;k++){
-                if(j>>k&1)continue;
-                int j1=1<<k|j;
-                for(__int128 x:v[k]){
-                    int mx=0;
-                    for(int l=0;l<100;l++)
-                        if(x>>l&1)mx=max(l%10,mx);
-                    for(int l=0;l<100&&x<<l>=0;l++){
-                        if(l%10+mx>=10)continue;
-                        if(~all&(x<<l))continue;
-                        if(~(x<<l)>>i&1)continue;
-                        if(x<<l&cur)continue;
-                        for(auto[key,val]:f[i][j]){
-                            if(x<<l&key)continue;
-                            __int128 key1=x<<l|key;
-                            int i1=i;
-                            while(i1<100&&(~all|key1)>>i1&1)i1++;
-                            f[i1][j1][key1]+=val;
-                        }
+    operator U128() const
+    {
+        U128 ret = 0;
+        for (auto [x, y] : cells)
+        {
+            ret |= (U128)1 << (x * 10 + y);
+        }
+        return ret;
+    }
+};
+int main()
+{
+    auto st = clock();
+    vector<Block> base = {
+        {{{0, 0}, {0, 1}, {1, 0}, {1, 1}, {2, 0}}},
+        {{{2, 0}, {1, 0}, {1, 1}, {0, 1}, {0, 2}}},
+        {{{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}}},
+        {{{0, 0}, {1, 0}, {0, 1}}},
+        {{{0, 0}, {1, 0}, {2, 0}, {2, 1}}},
+        {{{1, 0}, {1, 1}, {0, 1}, {2, 1}, {1, 2}}},
+        {{{0, 0}, {1, 0}, {0, 1}, {1, 1}}},
+        {{{0, 0}, {1, 0}, {0, 1}, {0, 2}, {1, 2}}},
+        {{{0, 0}, {1, 0}, {1, 1}, {1, 2}, {1, 3}}},
+        {{{0, 0}, {0, 1}, {0, 2}, {0, 3}}},
+        {{{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 2}}},
+        {{{0, 1}, {1, 1}, {2, 1}, {2, 0}, {3, 0}}}};
+    U128 boardMask = 0;
+    for (int r = 0; r < 10; r++)
+        for (int c = 0; c < 10; c++)
+            if (r + c < 10)
+                boardMask |= (U128)1 << (r * 10 + c);
+    vector<U128> orientations[12];
+    for (int i = 0; i < 12; i++)
+    {
+        vector<U128> pool;
+        Block cur = base[i];
+        for (int k = 0; k < 4; k++)
+        {
+            pool.push_back((U128)cur);
+            cur = cur.rotate();
+        }
+        cur = base[i].flip();
+        for (int k = 0; k < 4; k++)
+        {
+            pool.push_back((U128)cur);
+            cur = cur.rotate();
+        }
+        sort(pool.begin(), pool.end());
+        pool.erase(unique(pool.begin(), pool.end()), pool.end());
+        orientations[i] = move(pool);
+    }
+    static vector<U128> placements[12][100];
+    for (int i = 0; i < 12; i++)
+    {
+        for (auto p : orientations[i])
+        {
+            int mx = 0;
+            for (int i = 0; i < 100; i++)
+                if (p >> i & 1)
+                    mx = max(mx, i % 10);
+            for (int shift = 0; shift < 100; shift++)
+            {
+                if (shift % 10 + mx >= 10)
+                    continue;
+                U128 placement = p << shift;
+                if ((placement & ~boardMask) != 0)
+                    continue;
+                for (int pos = 0; pos < 100; pos++)
+                    if ((placement >> pos) & 1)
+                        placements[i][pos].push_back(placement);
+            }
+        }
+        for (int pos = 0; pos < 100; pos++)
+        {
+            auto &vec = placements[i][pos];
+            sort(vec.begin(), vec.end());
+            vec.erase(unique(vec.begin(), vec.end()), vec.end());
+        }
+    }
+    static unordered_map<U128, int> dp[101][1 << 12];
+    dp[0][0].reserve(64);
+    dp[0][0][0] = 1;
+    U128 filledUpto = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (i > 0 && (boardMask >> (i - 1) & 1))
+        {
+            filledUpto |= (U128)1 << (i - 1);
+        }
+        for (int used = 0; used < (1 << 12); used++)
+        {
+            auto &mapAt = dp[i][used];
+            if (mapAt.empty())
+                continue;
+            for (int k = 0; k < 12; k++)
+            {
+                if (used & (1 << k))
+                    continue;
+                int usedNext = used | (1 << k);
+                auto &nextMap = dp[i][usedNext];
+                if (nextMap.empty())
+                    nextMap.reserve(mapAt.size() * 4);
+                auto &posList = placements[k][i];
+                if (posList.empty())
+                    continue;
+                for (auto placement : posList)
+                {
+                    if (placement & filledUpto)
+                        continue;
+                    for (auto &[mask, val] : mapAt)
+                    {
+                        if (placement & mask)
+                            continue;
+                        U128 merged = mask | placement;
+                        int j = i;
+                        while (j < 100 && (((~boardMask) | merged) >> j & 1))
+                            j++;
+                        dp[j][usedNext][merged] += val;
                     }
                 }
             }
         }
-        if(all>>i&1)cur|=__int128(1)<<i;
+        if (boardMask >> i & 1)
+            filledUpto |= (U128)1 << i;
     }
-    cout<<f[100][(1<<12)-1][all]<<endl;
-    cerr<<1.*(clock()-st)/CLOCKS_PER_SEC;
+    cout << dp[100][(1 << 12) - 1][boardMask] << '\n';
+    cout << 1. * (clock() - st) / CLOCKS_PER_SEC << endl;
+    return 0;
 }
